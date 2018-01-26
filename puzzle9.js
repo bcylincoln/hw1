ecbdb42bbee3da4fa808037a6bf9ccf9b2f83c16
Rebecca <rebeccaabraham@berkeley.edu>
2018-01-26 15:39:27 -0800

    circle drawing func

--- puzzle9.js
+++ puzzle9.js
@@ -1,11 +1,56 @@
-function paintLine(direction, length) {
-	var i = 0
-	while (i < length) {
-		direction()
-		setColor("black")
+
+function doActionX(action, x) {
+	var i = 0;
+	while (i < x) {
+		action()
+		i++
 	}
 }
-paintLine(right, 9)
-paintLine(down, 9)
-paintLine(left, 9)
-paintLine(up, 9)
+
+
+
+function colorPixel(currX, currY, newX, newY) {
+	var relX = newX - currX;
+	var relY = newY - currY;
+	if (relX > 0) {
+		doActionX(right, relX);
+
+	} else {
+		doActionX(left, Math.abs(relX));
+	}
+
+	if (relY > 0) {
+		doActionX(up, relY)
+	} else {
+		doActionX(down, Math.abs(relY))
+	}
+	setColor("red")
+}
+
+function radians(degrees) {
+	return degrees * (Math.PI / 180);
+}
+
+function circle(centerX, centerY, radius) {
+	doActionX(right, centerX)
+	doActionX(down, centerY)
+
+	var angle = 0;
+	var currX = 0;
+	var currY = 0;
+	while(angle < 360) {
+		var x = Math.round(radius * Math.cos(radians(angle)))
+		var y = Math.round(radius * Math.sin(radians(angle)))
+		colorPixel(currX, currY, x, y);
+		currX = x;
+		currY = y;
+		angle += 5;
+	}
+		
+}
+
+circle(50, 50, 20);
+
+
+
+
